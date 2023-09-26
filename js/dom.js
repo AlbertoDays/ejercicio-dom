7858e876d74d638dde96d39404fdac9106b1545e
AlbertoDays <diazbarragan7@gmail.com>
2023-09-26 09:13:49 -0600

    Ejercicio link dom

--- js/dom.js
+++ js/dom.js
@@ -10,7 +10,20 @@ function init(){
     let p2Texto = document.createTextNode("Parrafo2");
     p2.appendChild(p2Texto);
 
-    
+    let p3 =document.createElement("p");
+    let p3Texto = document.createTextNode("Parrafo3");
+    p3.appendChild(p3Texto);
+
+    contenedor.appendChild(p1);
+    contenedor.appendChild(p2);
+    contenedor.appendChild(p3);
+
+    let enlace=document.createElement("a");
+    enlace.setAttribute("href","https://frontendmasters.com/");
+    let enlaceTexto = document.createTextNode ("Enlace");
+    enlace.appendChild(enlaceTexto);
+
+    contenedor.appendChild(enlace);
 
 }
 
